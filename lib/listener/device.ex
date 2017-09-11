67a81d2234861af23780e184cab796bf82178c7c
succcubbus <jannis.kaiser2@gmail.com>
2017-09-11 19:59:00 +0200

    improve device service, sort into folders

--- lib/listener/device.ex
+++ lib/listener/device.ex
@@ -1,8 +1,9 @@
 defmodule HomeAutomation.Device do
   alias HomeAutomation.Network, as: Network
+  alias HomeAutomation.EventQueue, as: EventQueue
   alias __MODULE__, as: Device
 
-  defstruct [:name, :ip, :mac, :vendor, online: true, last_online: DateTime.utc_now]
+  defstruct [:name, :ip, :mac, :vendor, :online, :last_online]
 
   def start_link do
     {:ok, _} = Agent.start_link(fn -> [] end, name: :device)
@@ -17,11 +18,13 @@ defmodule HomeAutomation.Device do
   end
 
   defp check_online do
+    hosts = Network.list_hosts()
+
     Agent.update(:device, fn devices -> 
       device_macs = Enum.map(devices, fn device -> device.mac end)
 
       # find out if device is already known by mac
-      {existing, new} = Enum.split_with(Network.list_hosts(), fn host -> host.mac in device_macs end)
+      {existing, new} = Enum.split_with(hosts, fn host -> host.mac in device_macs end)
 
       update_devices(devices, existing) ++ create_devices(new)
     end)
@@ -33,14 +36,24 @@ defmodule HomeAutomation.Device do
     Enum.map(devices, fn device -> # update device online status
       online = device.mac in host_macs
 
-      %{device |
+      device = %{device |
         online: online,
         last_online: if(online, do: DateTime.utc_now, else: device.last_online)}
+
+      if device.online and not online, do: EventQueue.call([:device, :offline, device])
+      if not device.online and online, do: EventQueue.call([:device, :online, device])
+
+      device
     end)
   end
 
   defp create_devices(hosts) do
-    Enum.map(hosts, &%Device{ip: &1.ipv4, mac: &1.mac, vendor: &1.vendor})
+    devices = Enum.map(hosts, &%Device{ip: &1.ipv4, mac: &1.mac, vendor: &1.vendor, online: true, last_online: DateTime.utc_now()})
+    Enum.each(devices, fn device ->
+      EventQueue.call([:device, :new, device])
+      EventQueue.call([:device, :online, device])
+    end)
+    devices
   end
 
   @doc"""
@@ -48,5 +61,13 @@ defmodule HomeAutomation.Device do
   """
   def list_devices do
     Agent.get(:device, fn devices -> devices end)
+  end
+
+  def find(name) do
+    Agent.get(:device, &Enum.find(&1, fn device -> device.name == name end))
+  end
+
+  def offline_duration(%Device{online: online, last_online: last_online}) do
+    if online, do: 0, else: DateTime.diff(DateTime.utc_now(), last_online, :second)
   end
 end
