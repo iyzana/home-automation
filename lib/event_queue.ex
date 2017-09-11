d5cd8a59d1facc53fabe2a9c9ac5dd3b63668a63
succcubbus <jannis.kaiser2@gmail.com>
2017-09-11 19:58:46 +0200

    add event queue matching

--- lib/event_queue.ex
+++ lib/event_queue.ex
@@ -1,20 +1,23 @@
 defmodule HomeAutomation.EventQueue do
   use GenServer
 
-  defmodule HomeAutomation.EventQueue.Event do
-    defstruct [:type, :action, :data]
-  end
-
   def start_link do
-    Agent.start_link(fn -> {} end, name: :event_listeners)
+    Agent.start_link(fn -> %{} end, name: :event_listeners)
   end
 
-  def register(event, callback) do
-    Agent.update(:event_listeners, &Map.update(&1, event, [], fn callbacks -> [callback | callbacks] end))
+  def register(matcher, callback) do
+    # append to callbacks if someone already registered, create a new entry otherwise
+    Agent.update(:event_listeners, &Map.update(&1, matcher, [callback], fn callbacks -> [callback | callbacks] end))
   end
 
   def call(event) do
-    Agent.get(:event_listeners, &Map.get(&1, event, []))
+    IO.puts "received event: " <> inspect(event)
+
+    Agent.get(:event_listeners, fn map ->
+      map
+      |> Enum.filter(fn {matcher, _} -> Enum.zip(matcher, event) |> Enum.all?(fn {m, e} -> m == e end) end)
+      |> Enum.flat_map(fn {_, callbacks} -> callbacks end)
+    end)
     |> Enum.each(fn callback -> callback.(event) end)
   end
 end
