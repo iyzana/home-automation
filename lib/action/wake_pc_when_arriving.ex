9c4904267aee85fbee73ef4e5db9cb0314688051
succcubbus <jannis.kaiser2@gmail.com>
2017-09-13 14:42:54 +0200

    add name to event registration

--- lib/action/wake_pc_when_arriving.ex
+++ lib/action/wake_pc_when_arriving.ex
@@ -3,14 +3,14 @@ defmodule HomeAutomation.WakePcWhenArriving do
   alias HomeAutomation.Device
   alias HomeAutomation.WakeOnLan
 
+  # todo: create a protocol/behaviour for actions to implement a name, match and run method
   def register do
     # wake the pc when the phone comes online
-    EventQueue.register [:device, :online], fn [_, _, dev] ->
+    EventQueue.register "wake pc when arriving", [:device, :online], fn [_, _, dev] ->
       if dev.name == "phone" do
         pc = Device.find("pc")
 
         if not pc.online and Device.offline_duration(pc) > 3600 do
-          IO.puts ":: wake pc when arriving"
           WakeOnLan.send(pc.mac)
         end
       end
--- lib/event_queue.ex
+++ lib/event_queue.ex
@@ -5,19 +5,23 @@ defmodule HomeAutomation.EventQueue do
     Agent.start_link(fn -> %{} end, name: :event_listeners)
   end
 
-  def register(matcher, callback) do
+  def register(name, matcher, callback) do
+    tuple = {name, callback}
     # append to callbacks if someone already registered, create a new entry otherwise
-    Agent.update(:event_listeners, &Map.update(&1, matcher, [callback], fn callbacks -> [callback | callbacks] end))
+    Agent.update(:event_listeners, &Map.update(&1, matcher, [tuple], fn callbacks -> [tuple | callbacks] end))
   end
 
   def call(event) do
-    IO.puts "received event: " <> inspect(event)
+    IO.puts "[<] " <> inspect(event)
 
     Agent.get(:event_listeners, fn map ->
       map
       |> Enum.filter(fn {matcher, _} -> Enum.zip(matcher, event) |> Enum.all?(fn {m, e} -> m == e end) end)
       |> Enum.flat_map(fn {_, callbacks} -> callbacks end)
     end)
-    |> Enum.each(fn callback -> callback.(event) end)
+    |> Enum.each(fn {name, callback} ->
+      IO.puts "[>] " <> name
+      callback.(event)
+    end)
   end
 end
