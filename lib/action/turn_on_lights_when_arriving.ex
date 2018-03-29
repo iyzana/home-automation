b022b80e6831af2248a7daefe10c2a72f58c34c7
succcubbus <jannis.kaiser2@gmail.com>
2018-03-29 23:07:14 +0200

    turn on lights when arriving

---
+++ lib/action/turn_on_lights_when_arriving.ex
@@ -0,0 +1,28 @@
+defmodule HomeAutomation.TurnOnLightsWhenArriving do
+  alias HomeAutomation.EventQueue
+  require Logger
+
+  @name "turn-on-lights-when-arriving"
+
+  # todo: create a protocol/behaviour for actions to implement a name, match and run method
+  def register do
+    # wake the pc when the phone comes online
+    EventQueue.register(@name, [:device, :online], fn [_, _, dev, old_dev] ->
+      {status, message} =
+        cond do
+          dev.name != "phone" ->
+            {:debug, "not the phone"}
+
+          true ->
+            Lifx.Client.devices()
+            |> Enum.filter(fn light -> light.label == "light" end)
+            |> Enum.each(&Lifx.Device.on/1)
+
+            {:info, "turning on light"}
+        end
+
+      symbol = if status == :info, do: "✓", else: "✗"
+      Logger.log(status, "#{@name} #{symbol} #{message}")
+    end)
+  end
+end
--- lib/actions.ex
+++ lib/actions.ex
@@ -1,7 +1,12 @@
 defmodule HomeAutomation.Actions do
   @spec register_all() :: :ok
   def register_all do
-    ["WakePcWhenArriving", "NotifyOnUnknownDevice", "TurnOffLightsWhenLeaving"]
+    [
+      "WakePcWhenArriving",
+      "NotifyOnUnknownDevice",
+      "TurnOffLightsWhenLeaving",
+      "TurnOnLightsWhenArriving"
+    ]
     |> Enum.map(&("Elixir.HomeAutomation." <> &1))
     |> Enum.map(&String.to_atom/1)
     |> Enum.each(fn module -> module.register() end)
